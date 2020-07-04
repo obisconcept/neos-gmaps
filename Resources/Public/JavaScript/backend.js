6160d443fd8947f0d5ba81f5b551e00f496266e9
Maximilian Schmidt <m.schmidt@obis-concept.de>
2020-07-04 18:37:18 +0200

    rebuilt assets

--- Resources/Public/JavaScript/backend.js
+++ Resources/Public/JavaScript/backend.js
@@ -1,19 +1,11 @@
 if (typeof document.addEventListener === 'function') {
-
-    document.addEventListener('Neos.PageLoaded', function(event) {
-        
-        initGoogleMaps();
-        
-    });
-    
-    document.addEventListener('Neos.NodeCreated', function(event) {
-        
-        if ($(event.detail.element).find('.google-map-canvas')) {
-            
-            initGoogleMaps();
-            
-        }
-        
-    });
-
+  document.addEventListener('Neos.PageLoaded', function (event) {
+    initGoogleMaps();
+  });
+  
+  document.addEventListener('Neos.NodeCreated', function (event) {
+    if ($(event.detail.element).find('.google-map-canvas')) {
+      initGoogleMaps();
+    }
+  });
 }
